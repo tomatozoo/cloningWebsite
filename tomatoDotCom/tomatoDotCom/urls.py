cf574ee07994d860c4d5986e400f82b69eb49704
Young Zoo Park <enkeejuniour@naver.com>
2022-01-13 23:25:52 +0900

    main 페이지

--- tomatoDotCom/tomatoDotCom/urls.py
+++ tomatoDotCom/tomatoDotCom/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 
 import tomatoDotCom.views
 from django.urls import path
-
+from django.conf.urls.static import static
+from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', tomatoDotCom.views.index, name='index'),
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
