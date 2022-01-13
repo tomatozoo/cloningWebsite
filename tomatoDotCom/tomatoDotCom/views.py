7e939533e23d476d19a12838bccc20adfce67a1b
Young Zoo Park <enkeejuniour@naver.com>
2022-01-13 19:22:21 +0900

    .

---
+++ tomatoDotCom/tomatoDotCom/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+def index(request):
+    # tomatoDotCom\tomatoDotCom\templates\tomatoDotCom\index copy.html
+    return render(request, 'templates/tomatoDotcom/index copy.html')
+
